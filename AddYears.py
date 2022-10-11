b2b754019ce2f0859cfb2c659c6def1ae08e2204
yashpauranik <107103139+yashpauranik@users.noreply.github.com>
2022-10-11 13:10:39 +0530

    Delete AddYears.py

--- AddYears.py
+++
@@ -1,7 +0,0 @@
-from datetime import date , datetime
-from dateutil.relativedelta import relativedelta
-date_1 = date.today()
-print(date_1)
-
-result = date_1 + relativedelta(years=+2)
-print(result)
